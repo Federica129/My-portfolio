cc37c9de30d20d21a1d5e5949281d1361b986f0c
Federica129 <schillacifederica99@gmail.com>
2022-08-22 21:19:55 +0200

    fix: style Hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,27 +1,38 @@
 import "./index.css";
 import { useEffect, useState } from "react";
-import photo from "../../assets/avatar.png";
+import photo from "../../assets/avatar1.png";
 
 function Hero({ myRef }) {
   const [isActive, setActive] = useState("");
+  const [notActive, setNotActive] = useState("");
 
   useEffect(() => {
     setActive("active");
+    setTimeout(() => {
+      setActive("");
+      setNotActive("notActive");
+    }, 6000);
   }, []);
 
   return (
     <div className="Hero" id="Hero" ref={myRef.refHero}>
       <div className="box">
         <div className="textMain">
-          <h3 className={`hi ${isActive}`}>Hi, I'm</h3>
-          <h1 className={`main_title ${isActive}`}>Federica Schillaci</h1>
-          <h2 className={`h2 ${isActive}`}>Front-end web developer,</h2>
+          <h3 className={`hi ${isActive}${notActive}`}>Hi, I'm</h3>
+          <h1 className={`main_title ${isActive}${notActive}`}>
+            Federica Schillaci
+          </h1>
+          <h2 className={`h2 ${isActive}${notActive}`}>
+            Front-end web developer,
+          </h2>
           <span></span>
-          <p className={`p ${isActive}`}>HTML/CSS/JAVASCRIPT/REACT</p>
+          <p className={`p ${isActive}${notActive}`}>
+            HTML/CSS/JAVASCRIPT/REACT
+          </p>
         </div>
       </div>
       <div className="myphoto">
-        <img className={`photo ${isActive}`} src={photo} alt="" />
+        <img className={`photo ${isActive}${notActive}`} src={photo} alt="" />
       </div>
     </div>
   );
